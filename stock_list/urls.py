34bc8ddc0deed9f130038ff4a236699a8d8db97d
htogether7 <htogether7@gmail.com>
2022-10-24 05:01:29 +0900

    FEAT: 원하는 주식 추가 삭제 기능 추가

--- stock_list/urls.py
+++ stock_list/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('news/<str:corpname>', views.getStockNews, name='news'),
     path('<str:corpname>/delete', views.deleteStock, name='delete_stock'),
     path('search/', views.searchStock, name='search_stock'),
+    path('<str:corpname>/add', views.addStock, name='add_stock')
+    # path()
     # path('mystock', views.getMyStock, name='mystock'),
 ]
--- stock_list/views.py
+++ stock_list/views.py
@@ -23,11 +23,20 @@ def getStockNews(request, corpname):
     # news = Newstbl.objects.all()
     corp_info = Stocktbl.objects.filter(symbol=corpname)
     corp_info_serializer = StockDataSerializer(corp_info, many=True)
+
     news = Newstbl.objects.filter(corp=corpname)
-    # print(news)
-    # news = Newstbl.objects.all()
     news_serializer = NewsSerializer(news, many=True)
-    context = {'news_list' : news_serializer.data, 'corp_info': corp_info_serializer.data}
+
+    my_stock = Mystock.objects.filter(username=request.user)
+    my_stock_serializer = MyStockSerializer(my_stock, many=True)
+
+    check_stock_in_my_stock = False
+    for stock in my_stock_serializer.data:
+        if stock['symbol'] == corpname.upper():
+            check_stock_in_my_stock = True
+            break
+
+    context = {'news_list' : news_serializer.data, 'corp_info': corp_info_serializer.data, 'check_stock_in_my_stock': check_stock_in_my_stock}
     # print(corp_info_serializer.data)
     return render(request, 'stock_list/stock_news.html', context)
 
@@ -39,17 +48,22 @@ def deleteStock(request, corpname):
 
 def searchStock(request):
     stock_name = request.GET['stock_name']
-    # print(stock_name.upper)
     entire_stock = Stocktbl.objects.all()
     entire_stock_serializer = StockDataSerializer(entire_stock, many=True)
     for data in entire_stock_serializer.data:
         if data['symbol'] == stock_name.upper():
             return redirect('stock_list:news', corpname=stock_name)
-    # print(entire_stock_serializer.data[0]['symbol'])
     if stock_name:
         return render(request, 'stock_list/stock_not_exist.html')
     else:
         return render(request, 'index.html')
+
+def addStock(request, corpname):
+    if request.user.is_authenticated:
+        new_stock = Mystock(username=request.user, symbol=corpname)
+        new_stock.save()
+    # print(request.user)
+    return redirect('stock_list:news', corpname=corpname)
 
 # def index(request):
 #     stock_list = Stocktbl.objects.order_by('id')
